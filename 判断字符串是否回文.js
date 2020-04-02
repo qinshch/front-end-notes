ff437ab4168f6363988e051ab32e1f3d4cdd2a9e
qinshch <qinshch@outlook.com>
2020-04-02 11:49:32 +0800

    make things into js file

--- 判断字符串是否回文.js
+++
@@ -1,7 +0,0 @@
-function isPalindrome(str){
-    let reg = /[\W_]/g,
-    newStr = str.replace(reg,"").toLowerCase(),
-    reverseStr = newStr.split("").reverse().join('');
-
-    return newStr === reverseStr;
-}
--- 数组求最大最小值.js
+++
@@ -1,4 +0,0 @@
-var arr = [6, 4, 1, 8, 2, 11, 23];
-console.log(Math.max.apply(null,arr));
-//Math.max(1,2,3,4)是这样用到
-//由于arr是数组，单独调用会返回NaN。
